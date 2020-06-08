769702ad37f6f25072712d2e81f7ff55863dda64
marlonconcenza <marlon.concenza@fmail.com>
2020-06-08 11:23:56 -0300

    ajuste na home page

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,9 +1,13 @@
 import path from 'path';
 
 module.exports = {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        host : 'ec2-18-235-97-230.compute-1.amazonaws.com',
+        user : 'jxuibyuvhayost',
+        password : '7a2ab8f5c9b840cf2b39098e6d975b20801cba741a6b13dfa6c2727d5554cebc',
+        database : 'dc7eg2t1jo64eg',
+        port: 5432
     },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,14 +1,26 @@
 import knex from 'knex';
-import path from 'path';
+//import path from 'path';
 
 //__dirname: Retorna o diretório do arquivo atual
 
-const connection = knex({
-    client: 'sqlite3',
+// const connection = knex({
+//     client: 'sqlite3',
+//     connection: {
+//         filename: path.resolve(__dirname, 'database.sqlite')
+//     },
+//     useNullAsDefault: true
+// });
+
+var connection = require('knex')({
+    client: 'pg',
+    version: '7.2',
     connection: {
-        filename: path.resolve(__dirname, 'database.sqlite')
-    },
-    useNullAsDefault: true
+        host : 'ec2-18-235-97-230.compute-1.amazonaws.com',
+        user : 'jxuibyuvhayost',
+        password : '7a2ab8f5c9b840cf2b39098e6d975b20801cba741a6b13dfa6c2727d5554cebc',
+        database : 'dc7eg2t1jo64eg',
+        port: 5432
+    }
 });
 
 export default connection;
